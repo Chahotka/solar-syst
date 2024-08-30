78e63e09a2e7e9c65600624c669fb1923befd6d5
Arslan Rasulov <magomrak@yandex.ru>
2024-08-30 15:40:13 +0200

    base

---
+++ src/components/canvas/Canvas.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import cl from '../../styles/canvas.module.css'
+
+
+const Canvas: React.FC = () => {
+  return (
+    <canvas className={cl.canvas}>
+
+    </canvas>
+  )
+}
+
+export default Canvas
